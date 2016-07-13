f5819ea2741fe9fe9a22a161f1c8d1b2b5219cc3
mmartinez326 <mmartinez3@mail.usf.edu>
2016-07-12 23:57:28 -0400

    Add files via upload

--- mQueue.h
+++ mQueue.h
@@ -1,64 +1,58 @@
-#ifndef MQUEUE_H
-#define MQUEUE_H
-
 #include "SLinkedList.h"
 template<class type>
-class mQueue :public SLinkedList<type> {
+class mQueue:public SLinkedList<type>{
 private:
-	Node<type> *tail;
+    Node<type> *tail;
 public:
-	mQueue() :tail(nullptr) {}
-	bool isEmpty() const {
-		return SLinkedList<type>::isEmpty();
-	}
-	void enqueue(const type & data) {
-		Node<type> *tmp = new Node<type>(data);
-		if (this->isEmpty()) {
-			SLinkedList<type>::head = tmp;
-			tail = tmp;
-			SLinkedList<type>::sz++;
-		}
-		else {
-			tail->next = tmp;
-			tail = tmp;
-			SLinkedList<type>::sz++;
-		}
-	}
+    mQueue():tail(nullptr){}
+    bool isEmpty() const{
+        return SLinkedList<type>::isEmpty();
+    }
+    void enqueue(const type & data){
+        Node<type> *tmp = new Node<type>(data);
+        if(this->isEmpty()){
+            SLinkedList<type>::head = tmp;
+            tail = tmp;
+            SLinkedList<type>::sz++;
+        }
+        else{
+            tail->next =tmp;
+            tail = tmp;
+            SLinkedList<type>::sz++;
+        }
+    }
 
-	type front() const {
-		if (this->isEmpty()) throw underflow_error("Empty Queue");
-		return SLinkedList<type>::head->data;
-	}
+    type front() const {
+        if (this->isEmpty()) throw underflow_error("Empty Queue");
+        return SLinkedList<type>::head->data;
+    }
 
-	type back() const {
-		if (this->isEmpty()) throw underflow_error("Empty Queue");
-		return tail->data;
-	}
+    type back() const {
+        if (this->isEmpty()) throw underflow_error("Empty Queue");
+        return tail->data;
+    }
 
-	type dequeue() {
-		type dat = this->front();
-		Node<type> *ptr = SLinkedList<type>::head;
-		SLinkedList<type>::head = ptr->next;
-		delete ptr;
-		SLinkedList<type>::sz--;
-		return dat;
-	}
+    type dequeue(){
+        type dat = this->front();
+        Node<type> *ptr = SLinkedList<type>::head;
+        SLinkedList<type>::head = ptr->next;
+        delete ptr;
+        SLinkedList<type>::sz--;
+        return dat;
+    }
 
-	int size() {
-		return SLinkedList<type>::sz;
-	}
+    int size(){
+        return SLinkedList<type>::sz;
+    }
 
-	void static display(mQueue<type> & Q) {
-		mQueue<type> Qtmp;
-		while (!Q.isEmpty()) {
-			type d = Q.dequeue();
-			Qtmp.enqueue(d);
-			cout << d << "->";
-		}
-		cout << "back" << endl;
-		Q = Qtmp;
-	}
+    void static display(mQueue<type> & Q){
+        mQueue<type> Qtmp;
+        while (!Q.isEmpty()){
+            type d = Q.dequeue();
+            Qtmp.enqueue(d);
+            cout << d << "->";
+        }
+        cout << "back"<< endl;
+        Q = Qtmp;
+    }
 };
-
-
-#endif
